870aa2b500f0ebb3cc6058f7fcc6160c97e636e2
´Pesis04 <augus.abraham@gmail.com>
2020-06-04 10:52:09 -0300

    ejercicio uno en mi ano

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,4 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
-function mostrar()
-{
-	alert("ok");
+function mostrar() {
+    alert("ME ENCANTA LA CHOTA LA PUTA MADRE");
 }
-
